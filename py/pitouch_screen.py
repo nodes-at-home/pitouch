42f5f82f01aca68890a6df6abd853c0e6f39aaab
Andreas <andreas@jungierek.de>
2019-10-19 14:49:52 +0200

    first working version

--- py/pitouch_screen.py
+++ py/pitouch_screen.py
@@ -3,51 +3,61 @@
 
 # junand 18.10.2019
 
-import os
 import paho.mqtt.client as mqtt
 import paho.mqtt.subscribe as subscribe
 import paho.mqtt.publish as publish
 
-TOPIC = "nodes@home/display/touchscreen/corridor"
+from rpi_backlight import Backlight;
+
+TOPIC = "nodes@home/display/pitouch/corridor"
 TOPIC_ONOFF = TOPIC + "/screen"
 TOPIC_ONOFF_STATE = TOPIC_ONOFF + "/state"
 TOPIC_BRIGHTNESS = TOPIC + "/brightness"
 TOPIC_BRIGHTNESS_STATE = TOPIC_BRIGHTNESS + "/state"
 TOPIC_PING = TOPIC + "/ping"
 
-BROKER = "nodesathome2"
+BROKER = "nodesathome1"
 
 QOS = 1
 RETAIN = True
 
+backlight = Backlight ()
+
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect ( client, userdata, flags, rc ):
-	print ( "Connected with result code " + str ( rc ) )
+    print ( "Connected with result code " + str ( rc ) )
 
-	client.subscribe ( TOPIC + "#" )
+    client.subscribe ( TOPIC + "/+" )
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message ( client, userdata, msg ):
 
-	print ( msg.topic + "->" + str ( msg.payload ) )
-	
-	if ( msg.topic == TOPIC_ONOFF ):
+    print ( msg.topic + "->" + str ( msg.payload ) )
 
-		if msg.payload == b'ON':
-			print ( "screen on" )
-			os.system ( 'sudo sh -c "echo 0 > /sys/class/backlight/rpi_backlight/bl_power"' )
-			client.publish ( TOPIC_ONOFF_STATE, payload = "ON", qos = QOS, retain = RETAIN )
-			
-		elif msg.payload == b'OFF':
-			print ( "screen off" )
-			os.system ( 'sudo sh -c "echo 1 > /sys/class/backlight/rpi_backlight/bl_power"' )
-			client.publish ( TOPIC_ONOFF_STATE, payload = "OFF", qos = QOS, retain = RETAIN )
-			
-	elif ( msg.topic == TOPIC_BRIGHTNESS ):
-		ossudo sh -c "echo '200' >> /sys/class/backlight/rpi_backlight/brightness"
-	
-	elif ( msg.topic == TOPIC_PING ):
-	
+    if ( msg.topic == TOPIC_ONOFF ):
+
+        if msg.payload == b'ON':
+            print ( "screen on" )
+            backlight.power = True
+            client.publish ( TOPIC_ONOFF_STATE, payload = "ON", qos = QOS, retain = RETAIN )
+            print ( "state published" )
+
+        elif msg.payload == b'OFF':
+            print ( "screen off" )
+            backlight.power = False
+            client.publish ( TOPIC_ONOFF_STATE, payload = "OFF", qos = QOS, retain = RETAIN )
+            print ( "state published" )
+
+    elif ( msg.topic == TOPIC_BRIGHTNESS ):
+        brightness = msg.payload.decode ( 'utf-8' )
+        print ( "screen brightness=" + brightness )
+        backlight.brightness = int ( brightness )
+        client.publish ( TOPIC_BRIGHTNESS_STATE, payload = brightness, qos = QOS, retain = RETAIN )
+        print ( "state published" )
+
+    elif ( msg.topic == TOPIC_PING ):
+        print ( "ping" )
+
 #
 # main
 #
