fe16a9ae5fc52fd294bc3dea284fc429bf932768
Andreas <andreas@jungierek.de>
2019-10-27 07:59:05 +0100

    added apds9960 and periodic call

--- py/pitouch_screen.py
+++ py/pitouch_screen.py
@@ -9,26 +9,66 @@ import paho.mqtt.publish as publish
 
 from rpi_backlight import Backlight;
 
+import threading
+import time
+
+from apds9960.const import *
+from apds9960 import APDS9960
+import RPi.GPIO as GPIO
+import smbus
+
 TOPIC = "nodes@home/display/pitouch/corridor"
+TOPIC_MQTT = TOPIC + "/state/mqtt"
 TOPIC_ONOFF = TOPIC + "/screen"
 TOPIC_ONOFF_STATE = TOPIC_ONOFF + "/state"
 TOPIC_BRIGHTNESS = TOPIC + "/brightness"
 TOPIC_BRIGHTNESS_STATE = TOPIC_BRIGHTNESS + "/state"
-TOPIC_PING = TOPIC + "/ping"
+TOPIC_PEIODIC = TOPIC + "/value/voltage"
+TOPIC_AMBIENT = TOPIC + "/value/ambient"
 
 BROKER = "nodesathome1"
+
+SLEEP_DURATION = 15 * 60
 
 QOS = 1
 RETAIN = True
 
+APDS_INT_PIN = 7
+
+apds = APDS9960 ( smbus.SMBus ( 1 ) )
+
+#------------------------------------------------------------------------------------------------------------------------
+def periodic ():
+    while ( True ):
+        print ( "periodic: send to topic=" + TOPIC_PEIODIC )
+        client.publish ( TOPIC_PEIODIC, payload = '{"value":1000,"unit":"mV"}', qos = QOS, retain = RETAIN )
+        ambient = apds.readAmbientLight ()
+        r = apds.readRedLight ()
+        g = apds.readGreenLight ()
+        b = apds.readBlueLight ()
+        json = '{"ambient":%s,"red":%s,"green":%s,"blue":%s,"unit":"Lux"}' % (ambient, r, g, b)
+        print ( "periodic: send %s to %s" % (json, TOPIC_AMBIENT) )
+        client.publish ( TOPIC_AMBIENT, payload = json, qos = QOS, retain = RETAIN )
+        b = int ( 20 + 80 * ambient / 10000 )
+        print ( "periodic: set brigthness=" + str ( b ) + " ambient=" + str ( ambient ) )
+        backlight.brightness = b
+        client.publish ( TOPIC_BRIGHTNESS_STATE, payload = b, qos = QOS, retain = RETAIN )
+        print ( "periodic: going sleep" )
+        time.sleep ( SLEEP_DURATION )
+
 backlight = Backlight ()
 
+#------------------------------------------------------------------------------------------------------------------------
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect ( client, userdata, flags, rc ):
-    print ( "Connected with result code " + str ( rc ) )
+    print ( "on_connect: Connected to " + BROKER + " with result code " + str ( rc ) )
 
+    client.publish ( TOPIC_MQTT, payload = "online", qos = QOS, retain = RETAIN )
     client.subscribe ( TOPIC + "/+" )
+    threading.Thread ( target = periodic ).start ()
+    print ( "on_connect: periodic thread started" )
 
+#------------------------------------------------------------------------------------------------------------------------
 # The callback for when a PUBLISH message is received from the server.
 def on_message ( client, userdata, msg ):
 
@@ -55,18 +95,28 @@ def on_message ( client, userdata, msg ):
         client.publish ( TOPIC_BRIGHTNESS_STATE, payload = brightness, qos = QOS, retain = RETAIN )
         print ( "state published" )
 
-    elif ( msg.topic == TOPIC_PING ):
-        print ( "ping" )
+#------------------------------------------------------------------------------------------------------------------------
+# def intH(channel):
+    # print("INTERRUPT")
 
+#------------------------------------------------------------------------------------------------------------------------
 #
 # main
 #
+
+GPIO.setmode ( GPIO.BOARD )
+#GPIO.setup ( APDS_INT_PIN, GPIO.IN )
+#GPIO.add_event_detect ( APDS_INT_PIN, GPIO.FALLING, callback = intH )
+
+apds.enableLightSensor ()
 
 client = mqtt.Client ()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect ( BROKER, 1883, 60 )
+client.will_set ( TOPIC_MQTT, payload = "offline", qos = QOS, retain = RETAIN )
+
+client.connect ( BROKER, 1883, SLEEP_DURATION )
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
